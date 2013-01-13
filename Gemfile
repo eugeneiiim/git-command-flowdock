274c6c25055012d93afc33aa8a5e18440d0672a7
Eugene Marinelli <eugeneiiim@gmail.com>
2013-01-12 17:03:45 -0800

    cleanup

--- Gemfile
+++
@@ -1,5 +0,0 @@
-source 'https://rubygems.org'
-
-gem "json"
-gem "redis", "~> 3.0.1"
-gem "pony"
--- bin/git-command-flowdock.rb
+++ bin/git-command-flowdock.rb
@@ -5,15 +5,13 @@ def flowdock_message(msg)
   url = URI.parse("https://#{ENV['FLOWDOCK_TOKEN']}:DUMMY@api.flowdock.com/flows/#{ENV['FLOWDOCK_ORG']}/#{ENV['FLOWDOCK_FLOW']}/messages")
   req = Net::HTTP::Post.new(url.path)
   req.basic_auth ENV['FLOWDOCK_TOKEN'], 'DUMMY'
-  req.set_form_data({ :event => 'message', :content => msg })
+  req.set_form_data({:event => 'message', :content => msg })
   res = Net::HTTP.new(url.host, url.port)
   res.use_ssl = true
   res.start {|http| http.request(req) }
 end
 
-puts ENV['GIT_REPO_URL']
-
 tmp = '/tmp/tmprepo'
 system("git clone #{ENV['GIT_REPO_URL']} #{tmp}")
-flowdock_message(`cd #{tmp} && git shortlog -ns`)
+flowdock_message(`cd #{tmp} && #{ENV['GIT_COMMAND']}`)
 system("rm -rf #{tmp}")
